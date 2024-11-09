028c65d6f5a6986f5b1ed96899ad1eaa104ed204
“Prashant2415” <prashantmendhejbp@gmail.com>
2024-11-10 01:12:28 +0530

    UI updates

---
+++ DailyStories/client/src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../components/style.css"
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+export default HomePage
